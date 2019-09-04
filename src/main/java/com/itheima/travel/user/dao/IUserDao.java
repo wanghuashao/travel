719db110cd0670be4ceb18a48c931005239c2054
whz <2573339869@qq.com>
2019-09-04 09:19:59 +0800

    33

--- src/main/java/com/itheima/travel/user/dao/IUserDao.java
+++ src/main/java/com/itheima/travel/user/dao/IUserDao.java
@@ -3,6 +3,7 @@ package com.itheima.travel.user.dao;
 import com.itheima.travel.user.domain.User;
 
 public interface IUserDao {
+
     User findUserInfoByUserName(String userName);
 
     int register(User user);
